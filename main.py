0b3a236ef8efd79f0e30e5aa69243cde09cf50a2
ashvini-dure <46552223+ashvini-dure@users.noreply.github.com>
2022-02-07 20:10:06 +0530

    New File

---
+++ main.py
@@ -0,0 +1,9 @@
+user = input("Enter Image name : ")
+lim = int(input("How many images you want? "))
+
+from bing_image_downloader import downloader  #importing the library
+
+query_string = user
+
+downloader.download(query_string, limit=lim,
+                    output_dir='dataset', adult_filter_off = True, force_replace=False, timeout=60)   #passing the arguments to the function  #printing absolute paths of the downloaded images
